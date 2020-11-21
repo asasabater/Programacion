765c3104b8a60c1173e6252059446ea189977906
asasabater <74676623+asasabater@users.noreply.github.com>
2020-11-22 00:21:37 +0100

    Add files via upload

---
+++ UD.3/Eje3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Eje3{
+    public static void main(String [] args) {
+    int a = 8;
+    int b = 3;
+    int resultado = 0;
+    res = (double) a / b;
+    System.out.printf("La división es: %f\n", res);
+    System.out.printf("La división entre %d y %d es igual a %f \n", a, b, res);
+    System.out.printf("%.2f %n", res);
+    System.out.printf("%5.2f %n", res);
+    System.out.printf("%7.3f %n", res);
+    System. System.out.printf .printf("%07.3f %n" "%07.3f %n", res);
+    System.out.printf("%10.4f %n", res);
+    System.out.printf ("%5.3f %n", res);
+    System.out.printf ("%10.5f %n", res);
+    System.out.printf("%010.0f %n", res);
+    }
+}
